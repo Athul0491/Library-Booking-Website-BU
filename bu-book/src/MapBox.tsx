708f60cd3e46412539ca92c28ae5db5a5106cce8
Colby Frey <cfrey@protonmail.com>
2025-06-12 20:02:09 -0400

    front end inital setup

---
+++ bu-book/src/MapBox.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+//import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+
+function MapBoxApp() {
+  const [count, setCount] = useState(0)
+
+  return(
+    <>
+    
+    
+    </>
+  )
+}
